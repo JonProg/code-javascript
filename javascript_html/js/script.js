a242b8b40dd508c92de817ffeb6c1866418e94be
jon_prog <jonashenri.aquino@gmail.com>
2023-10-25 20:19:20 -0300

    modified script.js

--- javascript_html/js/script.js
+++ javascript_html/js/script.js
@@ -1,1 +1,5 @@
-confirm('Fala Players, Tranquilos ?')
+let number1 = Number(prompt('Digite um número:'));
+let number2 = Number(prompt('Digite outro número:'));
+const result = number1 + number2;
+
+alert(`O resultado da conta foi: ${result}`);
