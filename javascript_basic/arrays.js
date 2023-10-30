3fc71d48ec0123376890619215a4b5c8a138ecc3
jon_prog <jonashenri.aquino@gmail.com>
2023-10-30 19:05:49 -0300

    add array.js

---
+++ javascript_basic/arrays.js
@@ -0,0 +1,11 @@
+
+const game_class = ['Arqueiro','Guerreiro','Mago', 'Assasino','Samurai'];
+
+// game_class.push('Vagabundo') -> maneira de adicionar algo na lista
+// game_class.unshift('Primeiro') -> adicionar um valor no começo da lista
+// game_class.pop() -> retira o último valor da lista
+// game_class.shift() -> retira o primeiro valor da lista
+// delete game_class[1] -> retira o valor de indice 1 e coloca um <empty item> no lugar
+// game_class.slice(0,4) -> fatiar a lista pegando mais de um valor igual a string
+// (game_class instanceof Array); -> para saber se a lista é realmente uma lista rsrs
+
