fd5b06dc1da8a667446f56fadb877024995f482f
jon_prog <jonashenri.aquino@gmail.com>
2023-11-07 16:00:18 -0300

    started programing logic in javascript

---
+++ javascript_basic/programing_logic.js
@@ -0,0 +1,22 @@
+/*
+Operadores de comparação
+
+> | < | >= | <=
+
+== (valor) X 
+=== (valor e tipo)
+!= (valor) X
+!== (valor e tipo)
+-----------------------
+Operadores Lógicos
+
+&& -> AND
+|| -> OR
+! -> NOT
+
+*/
+
+const corUser = null
+const corDefault = corUser || 'preto' // Caso o úsuario como coloque alguma cor ela será colocada como padrão
+console.log(corDefault)
+
