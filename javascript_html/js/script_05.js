a7830a04cb76d96d94c0fdefc70823a1801f7828
jon_prog <jonashenri.aquino@gmail.com>
2023-11-08 17:28:20 -0300

    finished ex004

--- javascript_html/js/script_05.js
+++ javascript_html/js/script_05.js
@@ -7,19 +7,26 @@ function myFunction(){
         const height = parseFloat(form.querySelector('#height').value);
         const weight = parseFloat(form.querySelector('#weight').value);
         const imc = weight / (height * height);
-        let msg = `Seu IMC é ${imc.toFixed(2)} ${height}`;
+        let msg = `Seu IMC é ${imc.toFixed(2)}`;
+        let color_msg
 
-        if(!height || !weight || height>=9999 || weight>=999){
+        if(!height || !weight || height>=999 || weight>=9999){
             msg = 'Altura ou Peso inválidos';
+            color_msg = "msg-obesity";
         }else{
+            color_msg = "msg-obesity";
+
             if(imc<18.5){
                 msg += ' (Abaixo do peso)';
+                color_msg = "msg-underweight";
     
             }else if (imc>=18.5 && imc<=24.9){
                 msg += ' (Peso normal)';
+                color_msg = "msg-normal";
                 
             }else if (imc>=25 && imc<=29.9){
                 msg +=' (Sobrepeso)';
+                color_msg = "msg-overweight";
     
             }else if (imc>=30 && imc<=34.9){
                 msg += ' (Obesidade grau 1)';
@@ -32,7 +39,7 @@ function myFunction(){
             }
         }
 
-        result.innerHTML = `<p>${msg}</p>`;
+        result.innerHTML = `<p class="${color_msg}">${msg}</p>`;
     };
 
     form.addEventListener('submit', receiveEvent);
