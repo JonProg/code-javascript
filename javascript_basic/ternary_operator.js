65a729b66fb51d65e5f4e04426b209801d4d1c1f
jon_prog <jonashenri.aquino@gmail.com>
2023-11-11 08:34:35 -0300

    add ternary_operator

---
+++ javascript_basic/ternary_operator.js
@@ -0,0 +1,8 @@
+const life = 100;
+const playerStatus = life <=0 ? 'Morto':'Vivo';
+
+console.log(playerStatus);
+
+// codicional ? caso true:caso false
+// Uma simplificação interessante só que achei que tem muito simbolo e pouca
+// sintaxe condicional tipo "if" e "else"
