66ab92e844a46a3f1040b19ec6e08aca3511c8fe
jon_prog <jonashenri.aquino@gmail.com>
2023-11-19 10:24:07 -0300

    add timer_script

--- javascript_html/js/timer_script.js
+++ javascript_html/js/timer_script.js
@@ -1,7 +1,37 @@
 // flyer photoshop
 
-const timer = document.querySelector('.timer');
-const p = document.createElement('p')
+const clock = document.querySelector('.clock');
+const play =  document.querySelector('.play');
+const pause = document.querySelector('.pause');
+const stop = document.querySelector('.stop');
+let num = 0;
 
-p.innerHTML = '00:00'
-timer.appendChild(p)
+
+function seeHour(num){
+    let newClock = new Date()
+    newClock.setHours(0,0,num)
+    return newClock.toLocaleTimeString('pt-BR', {hour12:false})
+}
+
+play.addEventListener('click', function(event){
+    play.disabled = true;
+
+    const timer = setInterval(function(){
+        num++
+        clock.innerHTML = seeHour(num);
+    }, 1000);
+
+    setInterval(pause.addEventListener('click', function(event){
+        clearInterval(timer);
+        play.disabled = false;
+    })
+    )
+    setTimeout(stop.addEventListener('click', function(event){
+            num = 0;
+            clock.innerHTML = seeHour(num);
+            clearInterval(timer);
+            play.disabled = false;
+        })
+    )
+});
+
