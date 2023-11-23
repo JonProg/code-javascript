fc260d59e5859414b877a2a53a7e8bb2ec08244a
jon_prog <jonashenri.aquino@gmail.com>
2023-11-23 19:45:01 -0300

    add js in calculator

---
+++ javascript_html/js/projects/calculator_script.js
@@ -0,0 +1,77 @@
+function createCalculator() {
+
+    return {
+      display: document.querySelector('.display'),
+  
+      start() {
+        this.clikButtons();
+        this.pressBackSpace();
+        this.pressEnter();
+      },
+  
+      pressBackSpace() {
+        this.display.addEventListener('keydown', e => {
+          if (e.keyCode === 8) {
+            e.preventDefault();
+            this.clearDisplay();
+          }
+        });
+      },
+  
+      pressEnter() {
+        this.display.addEventListener('keyup', e => {
+          if (e.keyCode === 13) {
+            this.realizaConta();
+          }
+        });
+      },
+  
+      realizaConta() {
+        let conta = this.display.value;
+  
+        try{
+            conta = eval(conta);
+
+            if(!conta) {
+                alert(`Conta inválida`);
+                return;
+            }
+
+        }catch(e) {
+            alert('Conta inválida');
+            return;
+        }
+
+        this.display.value = String(conta);
+      },
+  
+      clikButtons() {
+        document.addEventListener('click', e => {
+          const el = e.target;
+  
+          if(el.classList.contains('btn-num')) {
+            this.display.value += el.innerText
+          }
+  
+          if(el.classList.contains('btn-clear')) {
+            this.display.value = '';
+          }
+  
+          if(el.classList.contains('btn-del')) {
+            this.display.value = this.display.value.slice(0, -1);
+          }
+  
+          if(el.classList.contains('btn-eq')) {
+            this.realizaConta();
+          }
+  
+          this.display.focus();
+        });
+      },
+  
+    };
+  }
+  
+  const calculator = createCalculator();
+  calculator.start();
+  
