1f12f5423161fe3ae6c493059208bd0e7b7db117
jon_prog <jonashenri.aquino@gmail.com>
2023-11-29 17:13:08 -0300

    add created_class

---
+++ poo_javascript/created_class.js
@@ -0,0 +1,14 @@
+
+class Pessoa{
+    constructor(nome,sobrenome){ // Igual ao __init__ do python
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    falar(){ // Esse metodo é automaticamente colocado no prototype
+        console.log(`${this.nome} está falando`)
+    }
+}
+
+const p1 = Pessoa('jonas','henrique')
+console.log(p1)
