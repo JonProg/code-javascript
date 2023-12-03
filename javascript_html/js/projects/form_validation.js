132636a075f57c24605c7f7f83e222e9d7ce38ea
jon_prog <jonashenri.aquino@gmail.com>
2023-12-02 22:31:40 -0300

    started code js

---
+++ javascript_html/js/projects/form_validation.js
@@ -0,0 +1,6 @@
+
+const username = document.querySelector('#username')
+const surname = document.querySelector('#surname')
+const cpf = document.querySelector('#cpf')
+const password = document.querySelector('#password')
+const passwordConfirmation = document.querySelector('#password-confirmation')
