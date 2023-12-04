58b29d047517a56b176ae9b6fdd36f9ca56a52e7
jon_prog <jonashenri.aquino@gmail.com>
2023-12-04 04:29:01 -0300

    modified cpf_validator

--- advanced_objects/cpf_validator.js
+++ advanced_objects/cpf_validator.js
@@ -1,8 +1,28 @@
-function reduceDigit(array){
+
+function ValidatorCPF(cpf){
+    this.cpf = cpf;
+    this.cpfClean = cpf.replace(/\D+/g,'');
+}
+
+ValidatorCPF.prototype.validated = function(){
+    if (typeof(this.cpfClean) === 'undefined' || this.cpfClean.length < 11) return false;
+
+    let cpfArray = Array.from(this.cpfClean.slice(0, 9));
+    let firstDigit = this.reduceDigit(cpfArray);
+    let scondDigit = this.reduceDigit(cpfArray);
+
+    if (Number(this.cpfClean.slice(9, 11)) === (firstDigit*10+scondDigit)){
+        return true
+    }else{
+        return false
+    }
+}
+
+ValidatorCPF.prototype.reduceDigit = function(array){
     let multiplier = array.length;
     let digit = 11 - (array.reduce(function (ac,val){
         let digit = (Number(val)*(multiplier+1))
-        multiplier -=1
+        multiplier--
         return ac + digit
     },0)% 11);
 
@@ -10,19 +30,5 @@ function reduceDigit(array){
     return digit;
 }
 
-
-function cpfValidator(cpf){
-    let cpfClean = cpf.replace(/\D+/g,'');
-    let cpfArray = Array.from(cpfClean.slice(0, 9));
-
-    let firstDigit = reduceDigit(cpfArray);
-    let scondDigit = reduceDigit(cpfArray);
-
-    if (Number(cpfClean.slice(9, 11)) === (firstDigit*10+scondDigit)){
-        console.log(`${cpf} é um cpf válido`)
-    }else{
-        console.log(`${cpf} é um cpf inválido`)
-    }
-}
-
-cpfValidator('705.484.450-52');
+const cpf = new ValidatorCPF('705.484.450-52');
+console.log(cpf.validated());
